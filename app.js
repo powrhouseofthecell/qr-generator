a7371236c4d325472f63cff306ea75a41e7d194f
Zuhaib <theshahzuhaib96@gmail.com>
2020-10-04 15:56:22 +0530

    hmmm

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-//image-charts.com/chart
-// ?cht=<chart_type>
-// &chd=<chart_data>
-// &chs=<chart_size>
-// &...additional_parameters...
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let url;
 app.get('/', (req, res) => {
 	res.sendFile(`${__dirname}/index.html`);
 });
 
 app.post('/', (req, res) => {
-	value = req.body.value;
-	url = `https://image-charts.com/chart?chs=150x150&cht=qr&chl=${value}&choe=UTF-8`;
+	let value = req.body.value;
+	let url = `https://image-charts.com/chart?chs=150x150&cht=qr&chl=${value}&choe=UTF-8`;
 	res.redirect(url);
 });
 
